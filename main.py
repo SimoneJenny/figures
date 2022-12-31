87e7b777e54920af77f8177948cd9ab6a176cca3
Simone Jenny Kjær Hansen <42372635+SimoneJenny@users.noreply.github.com>
2022-12-31 12:13:52 +0100

    heart

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from turtle import *
+
+color("darkred", "red")
+begin_fill()
+pensize(6)
+left(50)
+forward(133)
+circle(50, 200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+time.sleep(7)
